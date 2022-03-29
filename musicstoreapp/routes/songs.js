be635b690b2299dffe2d97d1ceb20874209c810c
Pablo García Fernández <uo276824@uniovi.es>
2022-03-29 19:01:28 +0200

    SDI-2122-1008-8.5-Colecciones relacionadas y routers.

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -26,10 +26,7 @@ module.exports = function (app, songsRepository) {
     });
 
     app.get('/songs/add', function (req, res) {
-        if (req.session.user == null) {
-            res.redirect("/shop");
-            return;
-        }
+
         res.render("songs/add.twig");
     });
 
@@ -37,17 +34,9 @@ module.exports = function (app, songsRepository) {
         let response = 'id: ' + req.params.id;
         res.send(response);
     });
-    app.get('/songs/:kind/:id', function (req, res) {
-        let response = 'id: ' + req.params.id + '<br>'
-            + 'Tipo de música: ' + req.params.kind;
-        res.send(response);
-    });
 
     app.post('/songs/add', function (req, res) {
-        if (req.session.user == null) {
-            res.redirect("/shop");
-            return;
-        }
+
         let song = {
             title: req.body.title,
             kind: req.body.kind,
@@ -64,20 +53,18 @@ module.exports = function (app, songsRepository) {
                         if (err) {
                             res.send("Error al subir la portada de la canción")
                         } else {
-                            res.send("Agregada la canción ID: " + songId)
+                            if (req.files.audio != null) {
+                                let audio = req.files.audio;
+                                audio.mv(app.get("uploadPath") + '/public/audios/' + songId + '.mp3', function (err) {
+                                    if (err) {
+                                        res.send("Error al subir el audio");
+                                    } else {
+                                        res.send("Agregada la canción ID: " + songId);
+                                    }
+                                });
+                            }
                         }
                     })
-                } else {
-                    if (req.files.audio != null) {
-                        let audio = req.files.audio;
-                        audio.mv(app.get("uploadPath") + '/public/audios/' + songId + '.mp3', function (err) {
-                            if (err) {
-                                res.send("Error al subir el audio");
-                            } else {
-                                res.send("Agregada la canción ID: " + songId);
-                            }
-                        });
-                    }
                 }
             }
         });
@@ -103,6 +90,39 @@ module.exports = function (app, songsRepository) {
         });
     })
 
+    app.get('/songs/edit/:id', function (req, res) {
+        let filter = {_id: ObjectId(req.params.id)};
+        songsRepository.findSong(filter, {}).then(song => {
+            res.render("songs/edit.twig", {song: song});
+        }).catch(error => {
+            res.send("Se ha producido un error al recuperar la canción" + error)
+        });
+    })
+
+    app.post('/songs/edit/:id', function (req, res) {
+        let song = {
+            title: req.body.title,
+            kind: req.body.kind,
+            price: req.body.price,
+            author: req.session.user
+        }
+        let songId = req.params.id;
+        let filter = {_id: ObjectId(songId)};
+        //que no se cree un documento nuevo, si no existe
+        const options = {upsert: false}
+        songsRepository.updateSong(song, filter, options).then(result => {
+            step1UpdateCover(req.files, songId, function (result) {
+                if (result == null) {
+                    res.send("Error al actualizar la portada o el audio de la canción");
+                } else {
+                    res.send("Se ha modificado el registro correctamente");
+                }
+            });
+        }).catch(error => {
+            res.send("Se ha producido un error al modificar la canción " + error)
+        });
+    })
+
     app.get('/songs/:id', function (req, res) {
         let filter = {_id: ObjectId(req.params.id)};
         let options = {};
@@ -112,5 +132,53 @@ module.exports = function (app, songsRepository) {
             res.send("Se ha producido un error al buscar la canción " + error)
         });
     })
+
+    app.get('/publications', function (req, res) {
+        let filter = {author: req.session.user};
+        let options = {sort: {title: 1}};
+        songsRepository.getSongs(filter, options).then(songs => {
+            res.render("publication.twig", {songs: songs});
+        }).catch(error => {
+            res.send("Se ha producido un error al listar las publicaciones del usuario:" + error)
+        });
+    })
+
+    function step1UpdateCover(files, songId, callback) {
+        if (files && files.cover != null) {
+            let image = files.cover;
+            image.mv(app.get("uploadPath") + '/public/covers/' + songId + '.png', function (err) {
+                if (err) {
+                    callback(null);
+                    // ERROR
+                } else {
+                    step2UpdateAudio(files, songId, callback);
+                    // SIGUIENTE
+                }
+            });
+        } else {
+            step2UpdateAudio(files, songId, callback); // SIGUIENTE } };
+        }
+    };
+
+    function step2UpdateAudio(files, songId, callback) {
+        if (files && files.audio != null) {
+            let audio = files.audio;
+            audio.mv(app.get("uploadPath") + '/public/audios/' + songId + '.mp3', function (err) {
+                if (err) {
+                    callback(null); // ERROR
+                } else {
+                    callback(true); // FIN
+                }
+            });
+        } else {
+            callback(true); // FIN
+        }
+    };
+
+    app.get('/songs/:kind/:id', function (req, res) {
+        let response = 'id: ' + req.params.id + '<br>'
+            + 'Tipo de música: ' + req.params.kind;
+        res.send(response);
+    });
 };
 
